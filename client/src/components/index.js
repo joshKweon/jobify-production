8867a236d3818eb31ecec6c9294400efae268a83
joshKweon <76968487+joshKweon@users.noreply.github.com>
2023-06-01 15:26:41 -0700

    first commit

---
+++ client/src/components/index.js
@@ -0,0 +1,18 @@
+import FormRow from './FormRow'
+import Logo from './Logo'
+import Alert from './Alert'
+import Navbar from './Navbar'
+import BigSidebar from './BigSidebar'
+import SmallSidebar from './SmallSidebar'
+import FormRowSelect from './FormRowSelect'
+import SearchContainer from './SearchContainer'
+import JobsContainer from './JobsContainer'
+import StatsContainer from './StatsContainer'
+import StatsItem from './StatsItem'
+import ChartsContainer from './ChartsContainer'
+import Loading from './Loading'
+
+export {
+    Logo, FormRow, Alert, Navbar, BigSidebar, SmallSidebar, FormRowSelect,
+    SearchContainer, JobsContainer, StatsContainer, StatsItem, ChartsContainer, Loading
+}
---
+++ client/src/pages/Landing.js
@@ -0,0 +1,35 @@
+import main from '../assets/images/main.svg'
+import Wrapper from '../assets/wrappers/LandingPage'
+import { Logo } from '../components'
+import { Link, Navigate } from 'react-router-dom'
+import { useAppContext } from '../context/appContext'
+import React from 'react'
+
+const Landing = () => {
+  const { user } = useAppContext()
+  return (
+    <React.Fragment>
+      {user && <Navigate to='/' />}
+      <Wrapper>
+        <nav>
+          <Logo />
+        </nav>
+        <div className='container page'>
+          <div className='info'>
+            <h1>
+              job <span>tracking</span> App
+            </h1>
+            <p>
+              I'm baby meditation beard lomo tattooed slow-carb roof party bodega boys twee, bitters williamsburg crucifix.
+              Fam portland pinterest, leggings fit forage meh lumbersexual man bun polaroid tonx prism messenger bag.
+            </p>
+            <Link to='/register' className='btn btn-hero'>Login/Register</Link>
+          </div>
+          <img src={main} alt='job hunt' className='img main-img' />
+        </div>
+      </Wrapper>
+    </React.Fragment>
+  )
+}
+
+export default Landing
